86335c201691329048216451d7e512f86c05a336
Lauren Homan <lauren.homan@spc.ox.ac.uk>
2023-11-07 14:29:57 +0000

    Your branch is ahead of origin/dev by 1 commit

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -1,19 +1,26 @@
 #Script to estimate the model parameters using a linear approximation
 
-#library(dplyr)
+install.packages("dplyr")
+library(dplyr)
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experiment1.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<???) %>% mutate(N_log = log(N))
+data_subset1 <- growth_data %>% filter(t<1800) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
+#Defining the linear model for the first part of the graph (the exponential)
+#The intercept is 6.903e+00, the gradient is 9.990e-03
+
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>???)
+data_subset2 <- growth_data %>% filter(t>1800)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
+
+#Defining the model according to the plateau
+#
